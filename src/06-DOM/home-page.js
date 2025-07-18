00cd9f93d17de9d58ff294dae1b1624e3b27f251
Fatesaid <fatesaid56@gmail.com>
2025-07-18 09:40:03 +0200

    add homepage DOM

--- src/06-DOM/home-page.js
+++ src/06-DOM/home-page.js
@@ -1,14 +1,24 @@
 export function homePage() {
-  const container = document.querySelector(".container");
+  const content = document.querySelector(".content");
 
   const homePageOption = document.createElement("div");
   homePageOption.classList.add("home-page-option");
 
-  const titleDiv = document.createElement("div");
-  titleDiv.classList.add("title");
-  titleDiv.textContent = "Battleship";
+  const playerOption = document.createElement("div");
+  playerOption.classList.add("player-option");
 
-  homePageOption.appendChild(titleDiv);
+  const playerOneOption = document.createElement("button");
+  playerOneOption.classList.add("player-one-option");
+  playerOneOption.textContent = "1 Player";
 
-  container.appendChild(homePageOption);
+  const playerTwoOption = document.createElement("button");
+  playerTwoOption.classList.add("player-two-option");
+  playerTwoOption.textContent = "2 Players";
+
+  playerOption.appendChild(playerOneOption);
+  playerOption.appendChild(playerTwoOption);
+
+  homePageOption.appendChild(playerOption);
+
+  content.appendChild(homePageOption);
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import "./style.css";
-import { homePage } from "./06-Home-Page/home.js";
+import { homePage } from "./06-DOM/home-page";
 
 homePage();
