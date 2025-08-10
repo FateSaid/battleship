c1bedb1737392692a0b6cba101c15b66bc87fc78
Fatesaid <fatesaid56@gmail.com>
2025-08-10 18:26:40 +0200

    fix: remove other html besides index

--- dist/gameMode.main.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-/*!**********************************!*\
-  !*** ./src/pages/game-screen.js ***!
-  \**********************************/
-console.log("Game Screen");
-
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZ2FtZU1vZGUubWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90ZW1wbGF0ZS8uL3NyYy9wYWdlcy9nYW1lLXNjcmVlbi5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIkdhbWUgU2NyZWVuXCIpO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
--- dist/playerTwo.main.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-/*!*********************************!*\
-  !*** ./src/pages/two-player.js ***!
-  \*********************************/
-console.log("Two player");
-
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicGxheWVyVHdvLm1haW4uanMiLCJtYXBwaW5ncyI6Ijs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdGVtcGxhdGUvLi9zcmMvcGFnZXMvdHdvLXBsYXllci5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIlR3byBwbGF5ZXJcIik7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
